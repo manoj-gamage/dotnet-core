fe2299f4d98132ca929710c6782cd1e831fcfabe
Manoj <manoj.gamage@qtcm.com>
2024-02-19 01:46:47 -0600

    Wraping up module 7

--- source/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
+++ source/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -16,9 +16,29 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List() {
-            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "All Pies");
-            return View(pieListViewModel);
+        //public IActionResult List() {
+        //    PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "All Pies");
+        //    return View(pieListViewModel);
+        //}
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id)
